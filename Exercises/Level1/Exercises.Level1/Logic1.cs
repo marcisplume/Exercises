1fe8d109ce886e66e2e7480324dc12549dab1d1a
marcisplume <103065307+marcisplume@users.noreply.github.com>
2022-05-01 19:23:59 +0300

    Update Logic1.cs

--- Exercises/Level1/Exercises.Level1/Logic1.cs
+++ Exercises/Level1/Exercises.Level1/Logic1.cs
@@ -19,6 +19,7 @@ public class Logic1
     /// </summary>
     public bool CigarParty(int cigars, bool isWeekend)
     {
+        // parbaude GIT no WEB
         throw new NotImplementedException();
     }
 
